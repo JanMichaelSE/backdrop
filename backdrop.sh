2559e3843a807d08c8606ee2dbe1761ab22a3d74
Jan Montalvo <55473580+JanMichaelSE@users.noreply.github.com>
2023-10-21 17:58:22 -0400

    Added FZF support for easier image selection.

--- backdrop.sh
+++ backdrop.sh
@@ -1,10 +1,3 @@
-# FEATURES
-# 1.) Needs to run just using the command at first. "backdrop"
-# 2.) Needs to search in specific directories to list out images that can be selected as background
-# 3.) After selection:
-#   - MVP: Just set the background and exit.
-#   - IDEAL: Show preview and ask for confirmation before saving. If rejected revert to previous background.
-
 usage() {
     echo "Usage: ${0} [-rh] [-p PATH]"
     echo 'DESCRIPTION'
@@ -15,6 +8,7 @@ usage() {
     echo 'OPTIONS'
     echo "  -p, --path <PATH>    Set a custom path to find wallpaper images. If not provided, a default"
     echo '                       path will be used.'
+    echo '  -f, --fuzzy          Performs a fuzzy finding (Requires fzf).'
     echo '  -r, --revert         Reverts to the last wallpaper that was set prior to the most recent change.'
     echo "  -h, --help           Displays help information on how to use the ${0} command, listing all"
     echo '                       available options.'
@@ -40,18 +34,15 @@ set_wallpaper() {
     check_command_status "Changing background image"
 }
 
-# 1. Need to specify PATHS to search images under.
-PICTURES_PATH="$HOME/Pictures/wallpapers" # Not sure about wallpapers but whatever for now in Pictures.
-CONFIGS_PATH="$HOME/.config/backdrop/wallpapers"
-CUSTOM_PATH=""
 
 # This is how to read long and short options, the "--" is to know when we are done.
-OPTIONS=$(getopt -o p:rh -l path:,revert,help -- "$@")
+OPTIONS=$(getopt -o p:frh -l path:,fuzzy,revert,help -- "$@")
 check_command_status "Getting command options"
 
 # Reorder the arguments to ensure they are correct
 eval set -- "$OPTIONS"
 
+# * Provide a flag to just give a filename if the user knows it and automatically set that new bg image "--image or -i" (Flag name could change)
 while true; do
     case "$1" in
         -p|--path)
@@ -62,7 +53,11 @@ while true; do
                exit 1
             fi
             # TODO: Need to see how to make this persist? Config file maybe?
+            echo "New custom path to find images has been set!"
             exit 0
+            ;;
+        -f|--fuzzy)
+            IS_FUZZY_FINDING=true
             ;;
         -r|--revert)
             echo "Reverting image... STILL PENDING"
@@ -86,6 +81,9 @@ done
 # FUTURE TODO: Need to see how to manage CUSTOM_PATH to persist and provide it as an option.
 
 # Check if image directories exist, give priority to ".config".
+PICTURES_PATH="$HOME/Pictures/wallpapers"
+CONFIGS_PATH="$HOME/.config/backdrop/wallpapers"
+CUSTOM_PATH=""
 if [[ -d $CONFIGS_PATH ]]; then
     select_image_path $CONFIGS_PATH
 elif [[ -d $PICTURES_PATH ]]; then
@@ -98,27 +96,14 @@ else
     exit 1
 fi
 
-# Conver wallpapers to an array so we can reference them by index
-IFS=$'\n' WALLPAPERS_ARRAY=($WALLPAPERS)
-
-# Display options to user
-i=1
-for wallpaper in "${WALLPAPERS_ARRAY[@]}"; do
-    echo "$i) $wallpaper"
-    ((i++))
-done
-
 # Get user selection
-while true; do
-    read -p "Please select image background image (or press 'q' to quit): " REPLY
-    if [[ $REPLY == 'q' ]]; then
-       echo "Exiting backdrop..." 
-       exit 0
-    elif (( REPLY > 0 && REPLY <= ${#WALLPAPERS_ARRAY[@]} )); then
+# TODO: NEED TO REFACTOR THIS
+if [[ $IS_FUZZY_FINDING = 'true' ]]; then
+    while true; do
         PREVIOUS_WALLPAPER=$(gsettings get org.gnome.desktop.background picture-uri)
-        SELECTED_WALLPAPAER=${WALLPAPERS_ARRAY[$REPLY-1]}
+        SELECTED_WALLPAPER=$(find -L "$SELECTED_PATH" -maxdepth 1 -type f | awk -F '/' '{print $NF}' | fzf)
 
-        set_wallpaper "file://$SELECTED_PATH/$SELECTED_WALLPAPAER"
+        set_wallpaper "file://$SELECTED_PATH/$SELECTED_WALLPAPER"
 
         while true; do
             read -p "Want to save this change? [y/N]: " CHOICE
@@ -136,10 +121,50 @@ while true; do
                     ;;
             esac
         done     
-    else
-        echo "Invalid selection, please try again."
-    fi
-done
+    done
+else
+    # Conver wallpapers to an array so we can reference them by index
+    IFS=$'\n' WALLPAPERS_ARRAY=($WALLPAPERS)
+
+    # Display options to user
+    i=1
+    for wallpaper in "${WALLPAPERS_ARRAY[@]}"; do
+        echo "$i) $wallpaper"
+        ((i++))
+    done
+    while true; do
+        read -p "Please select image background image (or press 'q' to quit): " REPLY
+        if [[ $REPLY == 'q' ]]; then
+           echo "Exiting backdrop..." 
+           exit 0
+        elif (( REPLY > 0 && REPLY <= ${#WALLPAPERS_ARRAY[@]} )); then
+            PREVIOUS_WALLPAPER=$(gsettings get org.gnome.desktop.background picture-uri)
+            SELECTED_WALLPAPER=${WALLPAPERS_ARRAY[$REPLY-1]}
+
+            set_wallpaper "file://$SELECTED_PATH/$SELECTED_WALLPAPER"
+
+            while true; do
+                read -p "Want to save this change? [y/N]: " CHOICE
+                case "$CHOICE" in
+                    [yY])
+                        echo "Successfully changed background image."
+                        break 2
+                        ;;
+                    [nN]|"")
+                        set_wallpaper "$PREVIOUS_WALLPAPER"
+                        break
+                        ;;
+                    *)
+                        echo "Invalid input..."
+                        ;;
+                esac
+            done     
+        else
+            echo "Invalid selection, please try again."
+        fi
+    done
+fi
+
 
 exit 0
 
@@ -147,11 +172,8 @@ exit 0
 # * Improve Readme.md for usage on this tool and installation.
 # * Provide flags to revert the last image selected "--revert or -r"
 # * Provide a flag to just give a filename if the user knows it and automatically set that new bg image "--image or -i" (Flag name could change)
-# * Provide a Preview Functionality before saving.
-#   - This could re-use the revert functionality above. Function would be good.
 # * See how a slide show implementation could fit here.
 # * See why it doesn't work with zsh, but it does work in Bash? (Might need to migrate to Golang by then.)
-# * Need to see how to integrate "fzf" to fuzzy find the background image in the directory.
 #   - Then the user hits enter and it previews the image.
 #   - If confirmed the background will stay changed.
 #   - If denied, the background will revert to the one the user had.
