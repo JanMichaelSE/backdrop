fc7e24ea3c57fda95b267aad15b8756257867ef2
Jan Montalvo <55473580+JanMichaelSE@users.noreply.github.com>
2023-10-27 18:24:03 -0400

    Develop functional slideshow selecting images with fzf.

--- backdrop.sh
+++ backdrop.sh
@@ -33,8 +33,27 @@ check_command_status() {
 }
 
 select_image_path() {
-    SELECTED_PATH=${1}
-    WALLPAPERS=$(find -L "${1}" -maxdepth 1 -type f | awk -F '/' '{print $NF}')
+    local PICTURES_PATH="$HOME/Pictures/wallpapers"
+    local CONFIGS_PATH="$HOME/.config/backdrop/wallpapers"
+
+    # Chosen in order of priority.
+    if [[ -d "$BACKDROP_IMAGE_PATH" ]]; then
+        SELECTED_PATH=${BACKDROP_IMAGE_PATH}
+        WALLPAPERS=$(find -L "${BACKDROP_IMAGE_PATH}" -maxdepth 1 -type f | awk -F '/' '{print $NF}')
+    elif [[ -d "$CONFIGS_PATH" ]]; then
+        SELECTED_PATH=${CONFIGS_PATH}
+        WALLPAPERS=$(find -L "${CONFIGS_PATH}" -maxdepth 1 -type f | awk -F '/' '{print $NF}')
+    elif [[ -d $PICTURES_PATH ]]; then
+        SELECTED_PATH=${PICTURES_PATH}
+        WALLPAPERS=$(find -L "${PICTURES_PATH}" -maxdepth 1 -type f | awk -F '/' '{print $NF}')
+    else
+        echo "No valid directories found to list images. Please assure you have one of the following configured:"
+        echo "     - $CONFIGS_PATH"
+        echo "     - $PICTURES_PATH"
+        echo '     - Set a custom path with the "--path" or "-p" flag.'
+        echo ""
+        exit 1
+    fi
 }
 
 get_previous_wallpaper() {
@@ -49,6 +68,7 @@ set_wallpaper() {
     if gsettings list-schemas | grep -iq mate.background; then
         gsettings set org.mate.background picture-filename "$1"
     elif gsettings list-schemas | grep -iq gnome.desktop.background; then
+        echo "Setting: $1"
         gsettings set org.gnome.desktop.background picture-uri "file://$1"
         gsettings set org.gnome.desktop.background picture-uri-dark "file://$1"
     fi
@@ -103,8 +123,94 @@ set_custom_path() {
     fi
 }
 
+setup_slideshow() {
+    # Selects the Image path and EXPOSES:
+    # * SELECTED_PATH
+    # * SELECTED_WALLPAPER
+    select_image_path
+
+    local PREVIOUS_WALLPAPER=$(get_previous_wallpaper)
+    local SELECTED_WALLPAPERS=$(find -L "$SELECTED_PATH" -maxdepth 1 -type f | awk -F '/' '{print $NF}' | fzf --layout=reverse --multi)
+    IFS=$'\n' local WALLPAPERS_ARRAY=($SELECTED_WALLPAPERS)
+
+    # Ask user for duration per slide
+    read -p "What should be the duration per slide? (In Seconds): " SLIDE_DURATION
+
+    # Create the slideshow folder if it doesn't exist
+    local SLIDESHOW_PATH="$HOME/.local/share/gnome-background-properties" 
+    local SLIDESHOW_CONFIG_PATH="$HOME/.local/share/backgrounds/backdrop_settings"
+    if [[ ! -d "$SLIDESHOW_PATH"  || ! -d "$SLIDESHOW_CONFIG_PATH" ]]; then
+       mkdir -p "$SLIDESHOW_PATH"
+       mkdir -p "$SLIDESHOW_CONFIG_PATH"
+    fi
+
+    local SLIDESHOW_FILE="$SLIDESHOW_PATH/backdrop_slideshow.xml"
+    local SLIDESHOW_CONFIG_FILE="$SLIDESHOW_CONFIG_PATH/backdrop_settings.xml"
+
+    # Create First Xml file that holds Slideshow images
+    echo '<?xml version="1.0" encoding="UTF-8"?>' > "$SLIDESHOW_FILE"
+    echo '<!DOCTYPE wallpapers SYSTEM "gnome-wp-list.dtd">' >> "$SLIDESHOW_FILE"
+    echo '<wallpapers>' >> "$SLIDESHOW_FILE"
+
+    for wallpaper in $WALLPAPERS_ARRAY; do
+        echo '  <wallpaper>' >> "$SLIDESHOW_FILE"
+        echo "    <name>Backdrop Slideshow</name>" >> "$SLIDESHOW_FILE"
+        echo "    <filename>$SLIDESHOW_CONFIG_FILE</filename>" >> "$SLIDESHOW_FILE"
+        echo '    <options>zoom</options>' >> "$SLIDESHOW_FILE"
+        echo '    <pcolor>#2c001e</pcolor>' >> "$SLIDESHOW_FILE"
+        echo '    <scolor>#2c001e</scolor>' >> "$SLIDESHOW_FILE"
+        echo '    <shade_type>solid</shade_type>' >> "$SLIDESHOW_FILE"
+        echo '  </wallpaper>' >> "$SLIDESHOW_FILE"
+    done
+
+    echo '</wallpapers>' >> "$SLIDESHOW_FILE"
+
+    # Create Second Xml file that holds Slideshow configuration
+    echo '<background>' > "$SLIDESHOW_CONFIG_FILE"
+    echo '  <starttime>' >> "$SLIDESHOW_CONFIG_FILE"
+    echo '    <year>2012</year>' >> "$SLIDESHOW_CONFIG_FILE"
+    echo '    <month>01</month>' >> "$SLIDESHOW_CONFIG_FILE"
+    echo '    <day>01</day>' >> "$SLIDESHOW_CONFIG_FILE"
+    echo '    <hour>00</hour>' >> "$SLIDESHOW_CONFIG_FILE"
+    echo '    <minute>00</minute>' >> "$SLIDESHOW_CONFIG_FILE"
+    echo '    <second>00</second>' >> "$SLIDESHOW_CONFIG_FILE"
+    echo '  </starttime>' >> "$SLIDESHOW_CONFIG_FILE"
+
+    local TOTAL_LENGTH=$((${#WALLPAPERS_ARRAY[@]} - 1))
+    for index in ${!WALLPAPERS_ARRAY[@]}; do
+      echo '  <static>' >> "$SLIDESHOW_CONFIG_FILE"
+      echo "    <duration>${SLIDE_DURATION}.0</duration>" >> "$SLIDESHOW_CONFIG_FILE"
+      echo "    <file>$SELECTED_PATH/${WALLPAPERS_ARRAY[$index]}</file>" >> "$SLIDESHOW_CONFIG_FILE"
+      echo '  </static>' >> "$SLIDESHOW_CONFIG_FILE"
+      echo '  <transition>' >> "$SLIDESHOW_CONFIG_FILE"
+      echo '    <duration>0.5</duration>' >> "$SLIDESHOW_CONFIG_FILE"
+      echo "    <from>$SELECTED_PATH/${WALLPAPERS_ARRAY[$index]}</from>" >> "$SLIDESHOW_CONFIG_FILE"
+      echo "    <to>$SELECTED_PATH/${WALLPAPERS_ARRAY[$(($index + 1))]}</to>" >> "$SLIDESHOW_CONFIG_FILE"
+      echo '  </transition>' >> "$SLIDESHOW_CONFIG_FILE"
+      echo '  <static>' >> "$SLIDESHOW_CONFIG_FILE"
+      echo "    <duration>${SLIDE_DURATION}.0</duration>" >> "$SLIDESHOW_CONFIG_FILE"
+      echo "    <file>$SELECTED_PATH/${WALLPAPERS_ARRAY[$(($index + 1))]}</file>" >> "$SLIDESHOW_CONFIG_FILE"
+      echo '  </static>' >> "$SLIDESHOW_CONFIG_FILE"
+
+      # Break early from for loop
+      if [[ $((index + 1)) -eq $TOTAL_LENGTH ]]; then
+          break 2
+      fi
+    done
+
+    # Transition again back to the first image.
+    echo '  <transition>' >> "$SLIDESHOW_CONFIG_FILE"
+    echo '    <duration>0.5</duration>' >> "$SLIDESHOW_CONFIG_FILE"
+    echo "    <from>$SELECTED_PATH/${WALLPAPERS_ARRAY[$TOTAL_LENGTH]}</from>" >> "$SLIDESHOW_CONFIG_FILE"
+    echo "    <to>$SELECTED_PATH/${WALLPAPERS_ARRAY[0]}</to>" >> "$SLIDESHOW_CONFIG_FILE"
+    echo '  </transition>' >> "$SLIDESHOW_CONFIG_FILE"
+    echo '</background>' >> "$SLIDESHOW_CONFIG_FILE"
+    
+    set_wallpaper $SLIDESHOW_CONFIG_FILE
+}
+
 # This is how to read long and short options, the "--" is to know when we are done.
-OPTIONS=$(getopt -o p:fh -l path:,fuzzy,help,uninstall -- "$@")
+OPTIONS=$(getopt -o p:fsh -l path:,fuzzy,slideshow,help,uninstall -- "$@")
 check_command_status "Getting command options"
 
 # Reorder the arguments to ensure they are correct
@@ -128,6 +234,12 @@ while true; do
         -f|--fuzzy)
             IS_FUZZY_FINDING=true
             ;;
+        -s|--slideshow)
+            # Configure slide show here.
+            echo "Starting slideshow configuration"
+            setup_slideshow
+            exit 0
+            ;;
         --uninstall)
             echo "Uninstalling Backdrop..."
             "$HOME/.backdrop/scripts/uninstall.sh"
@@ -143,25 +255,10 @@ while true; do
     shift # Move to next option
 done
 
-# Check if image directories exist.
-PICTURES_PATH="$HOME/Pictures/wallpapers"
-CONFIGS_PATH="$HOME/.config/backdrop/wallpapers"
-
-# Chosen in order of priority.
-if [[ -d "$BACKDROP_IMAGE_PATH" ]]; then
-    select_image_path $BACKDROP_IMAGE_PATH
-elif [[ -d "$CONFIGS_PATH" ]]; then
-    select_image_path $CONFIGS_PATH
-elif [[ -d $PICTURES_PATH ]]; then
-    select_image_path $PICTURES_PATH
-else
-    echo "No valid directories found to list images. Please assure you have one of the following configured:"
-    echo "     - $CONFIGS_PATH"
-    echo "     - $PICTURES_PATH"
-    echo '     - Set a custom path with the "--path" or "-p" flag.'
-    echo ""
-    exit 1
-fi
+# Selects the Image path and EXPOSES:
+# * SELECTED_PATH
+# * SELECTED_WALLPAPER
+select_image_path
 
 # Get user selection
 if [[ $IS_FUZZY_FINDING = 'true' ]]; then
